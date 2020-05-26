588b7e6fe3351b8a846a7677b7ad4a386349ffe9
Sam Mousa <sammousa@Sams-MacBook-Pro.local>
2020-05-26 16:40:33 -0500

    js

---
+++ assignment/Text Decorator/Text Decorator_files/decoratemytext.js
@@ -0,0 +1,34 @@
+function alertFn(){
+alert("Hello, world");
+}
+
+function biggerFont() {
+    var x = document.getElementById("biling_checkbox").checked;
+    let element = document.getElementById("user_text");
+    if(x){
+        element.classList.add("bigger_font", "textarea_color");
+    }else{
+        element.classList.add("bigger_font");
+    }
+}
+
+function biggerFontTimer() {
+    var x = document.getElementById("biling_checkbox").checked;
+    let element = document.getElementById("user_text");
+
+    var style = window.getComputedStyle(element).fontSize;
+    var fontSize = parseInt(style); 
+    //alert(fontSize);
+
+    if(x){
+        element.classList.add("textarea_color");
+        element.style.fontSize = (fontSize + 2) + 'pt';
+    }else{
+        element.style.fontSize = (fontSize + 2) + 'pt';
+    }
+}
+
+function biggerFontTimerInterval(){
+    setInterval(biggerFontTimer,500);
+}
+
