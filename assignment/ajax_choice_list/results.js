f041c3ea26d57fa57c92c27fa858f53095a10048
Sam Mousa <sammousa@Sams-MacBook-Pro.local>
2020-06-03 15:49:30 -0500

    ajax8

--- assignment/ajax_choice_list/results.js
+++ assignment/ajax_choice_list/results.js
@@ -1,10 +1,9 @@
 'usestric'
-$(document).ready(function(){
+$(function(){
     $('#chooseList').change(function(){
-        var path = '../ajax_choice_list/src/' + $("#hw").val();
-        alert($("#hw").val());
+        var path = '../ajax_choice_list/src/' + $("#chooseList").val();
         $.ajax({
-            url: '../ajax_choice_list/src/song.txt',
+            url: path,
             type: "GET",
             success: ajaxSuccess,
             error: ajaxFailure
@@ -18,5 +17,3 @@ function ajaxSuccess(data) {
 function ajaxFailure(xhr, status, exception) {
     console.log(xhr, status, exception);
 }
-
-$.get("https://www.google.com/images/branding/product/ico/googleg_lodp.ico");
