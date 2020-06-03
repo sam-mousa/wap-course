2c21dd66b85f12d1c61d7dd0a2bee7b790d4d488
Sam Mousa <sammousa@Sams-MacBook-Pro.local>
2020-06-03 17:05:52 -0500

    posts2

--- assignment/ajax_choice_list/results.js
+++ assignment/ajax_choice_list/results.js
@@ -2,9 +2,12 @@
 $(function(){
     $('#chooseList').change(function(){
         var path = '../ajax_choice_list/src/' + $("#chooseList").val();
-        $.get(path).done($("#output").val(data)).fail(ajaxFailure);
+        $.get(path).done(ajaxSuccess).fail(ajaxFailure);
     });
 });
+function ajaxSuccess(data) {
+    $("#output").val(data);
+}
 
 function ajaxFailure(xhr, status, exception) {
     console.log(xhr, status, exception);
